c6e4c5737d06e97fe20778593ac851b02b9bae3b
Albert Chen <33033663+albert-hen@users.noreply.github.com>
2025-01-14 00:28:21 -0800

    boop game class implementation

--- boop/BoopGame.py
+++ boop/BoopGame.py
@@ -5,7 +5,16 @@ sys.path.append("..")
 from Game import Game
 from .BoopLogic import GameState, STATE_WAITING_FOR_GRADUATION_CHOICE, STATE_WAITING_FOR_PLACEMENT
 import numpy as np
+from enum import Enum
 
+class MoveType(Enum):
+    PLACE_KITTEN = 0
+    PLACE_CAT = 1
+    SINGLE_GRADUATION = 2
+    HORIZONTAL_TRIPLE_GRADUATION = 3
+    VERTICAL_TRIPLE_GRADUATION = 4
+    DIAGONAL_TRIPLE_GRADUATION_UP = 5
+    DIAGONAL_TRIPLE_GRADUATION_DOWN = 6
 
 class Game:
     """
@@ -38,7 +47,7 @@ class Game:
         Returns:
             (x,y): a tuple of board dimensions
         """
-        return (8, 8)
+        return (6, 6)
 
     def getActionSize(self):
         """
@@ -65,27 +74,86 @@ class Game:
             nextBoard: board after applying action
             nextPlayer: player who plays in the next turn (should be -player)
         """
-        # translate tensor to GameState
-        # make sure to fill in the board with the correct state
-        # if game state is place piece then place piece, 
-        # check if graduation choice is needed
-        # if so return the graduation choice state
-        # otherwise return the next state with the turn switched
-        st = self.__tensor_to_game_state(board)
-        if st.state_mode == STATE_WAITING_FOR_PLACEMENT:
+        # Action is an index of the action space
+        # Translate the action index to a move
+        # Apply the move to the board
+        # If the resulting state is a graduation choice, return tensor state with same player
+        # If the resulting state is a placement choice, return tensor state with -player
+        game_state = self.__tensor_to_game_state(board)
+        move_location, move_type = self.__action_to_move(action)
+        if move_type in [MoveType.PLACE_KITTEN, MoveType.PLACE_CAT]:
+            game_state.place_piece(move_location, move_type)
+        else:
+            # graduation choice
+            row, col = move_location
+            if move_type == MoveType.SINGLE_GRADUATION:
+                game_state.choose_graduation((move_location))
+            elif move_type == MoveType.HORIZONTAL_TRIPLE_GRADUATION:
+                game_state.choose_graduation(((row, col, (row, col-1), (row, col+1))))
+            elif move_type == MoveType.VERTICAL_TRIPLE_GRADUATION:
+                game_state.choose_graduation(((row, col, (row-1, col), (row+1, col))))
+            elif move_type == MoveType.DIAGONAL_TRIPLE_GRADUATION_UP:
+                game_state.choose_graduation(((row, col, (row+1, col-1), (row-1, col+1))))
+            elif move_type == MoveType.DIAGONAL_TRIPLE_GRADUATION_DOWN:
+                game_state.choose_graduation(((row, col, (row-1, col-1), (row+1, col+1))))
+        if game_state.state_mode == STATE_WAITING_FOR_GRADUATION_CHOICE:
+            return (self.__game_state_to_tensor(game_state), player)
+        else:
+            return (self.__game_state_to_tensor(game_state), -player)
 
     def __action_to_move(self, action):
         """
-        6x6  place kitten
-        6x6  place cat
-        6x6    single graduation options
-        6x4    horizontal triple graduation
-        4x6    vertical triple graduation
-        4x4     / diagonal triple graduation
-        4x4     \ diagonal triple graduation
-        """
-        action_space = [6*6, 6*6, 6*6, 6*4, 4*6, 4*4, 4*4]
+        Converts an action index into a move tuple.
 
+        Input:
+            action: index of action within action space
+
+        Returns:
+            move: tuple of move information
+
+        Example:
+        0 -> ((0,0), MoveType.PLACE_KITTEN)
+        ...
+        35 -> ((5,5), MoveType.PLACE_KITTEN)
+
+        36 -> ((0,0), MoveType.PLACE_CAT)
+        ...
+        71 -> ((5,5), MoveType.PLACE_CAT)
+
+        187 -> ((4,4), MoveType.DIAGONAL_TRIPLE_GRADUATION_DOWN)
+
+        6x6    place kitten                       
+        6x6    place cat                      
+        6x6    single graduation options    
+        6x4    horizontal triple graduation 
+        4x6    vertical triple graduation   
+        4x4    / diagonal triple graduation
+        4x4    \ diagonal triple graduation
+        """
+        
+        action_space = [6*6, 6*6, 6*6, 6*4, 4*6, 4*4, 4*4]
+        action_space_cumsum = np.cumsum(action_space)
+
+        if action < action_space_cumsum[0]:
+            return ((action // 6, action % 6), MoveType.PLACE_KITTEN)
+        elif action < action_space_cumsum[1]:
+            action -= action_space_cumsum[0]
+            return ((action // 6, action % 6), MoveType.PLACE_CAT)
+        elif action < action_space_cumsum[2]:
+            action -= action_space_cumsum[1]
+            return ((action // 6, action % 6), MoveType.SINGLE_GRADUATION)
+        elif action < action_space_cumsum[3]:
+            action -= action_space_cumsum[2]
+            return ((action // 4, 1+(action % 4)), MoveType.HORIZONTAL_TRIPLE_GRADUATION)
+        elif action < action_space_cumsum[4]:
+            action -= action_space_cumsum[3]
+            return ((1+(action // 6), action % 6), MoveType.VERTICAL_TRIPLE_GRADUATION)
+        elif action < action_space_cumsum[5]:
+            action -= action_space_cumsum[4]
+            return ((1+(action // 4), 1+(action % 4)), MoveType.DIAGONAL_TRIPLE_GRADUATION_UP)
+        else:
+            action -= action_space_cumsum[5]
+            return ((1+(action // 4), 1+(action % 4)), MoveType.DIAGONAL_TRIPLE_GRADUATION_DOWN)
 
     def __tensor_to_game_state(self, board):
         st = GameState()
@@ -111,6 +179,9 @@ class Game:
             st.state_mode = STATE_WAITING_FOR_GRADUATION_CHOICE
 
         return st
+    
+    def __game_state_to_tensor(self, gamestate: GameState):
+
 
     def getValidMoves(self, board, player):
         """
